a360a6961961d44542fc29cf88721c107e009bc5
Lh <lihao1017@qq.com>
2023-07-13 21:56:52 +0800

    fix routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -12,6 +12,4 @@ func UserRoutes(incomingRoutes *gin.Engine) {
 	// incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:userId", controllers.GetUser())
-	incomingRoutes.POST("/users/signup", controllers.SignUp())
-	incomingRoutes.POST("/users/login", controllers.Login())
 }
