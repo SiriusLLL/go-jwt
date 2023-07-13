c8b82bbf60285dafd6024a0d49f61cd72d95403d
Lh <lihao1017@qq.com>
2023-07-13 15:10:26 +0800

    update userRouter.go

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,13 +2,15 @@ package routes
 
 import (
 	"github.com/SiriusLLL/go-jwt/controllers"
-	"github.com/SiriusLLL/go-jwt/middleware"
+	//"github.com/SiriusLLL/go-jwt/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.Authenticate())
+	// incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:userId", controllers.GetUser())
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
 }
