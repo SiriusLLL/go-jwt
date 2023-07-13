8398492f7979fa6f18a76e8ecb2a6e5403c7a1ed
Lh <lihao1017@qq.com>
2023-07-13 16:30:16 +0800

    middleware

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/SiriusLLL/go-jwt/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintln("no authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("firstName", claims.FirstName)
+		c.Set("lastName", claims.LastName)
+		c.Set("userId", claims.Uid)
+		c.Set("userType", claims.UserType)
+		c.Next()
+	}
+}
