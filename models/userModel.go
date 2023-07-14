741e7cf8a40ea50035825511f1e2410aa4487c20
Lh <lihao1017@qq.com>
2023-07-14 16:11:09 +0800

    fix bug of json tag

--- models/userModel.go
+++ models/userModel.go
@@ -9,13 +9,13 @@ import (
 // 定义用户结构体
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
-	FirstName    *string            `json:"firstName" validate:"required, min=2, max=100"`
-	LastName     *string            `json:"lastName" validate:"required, min=2, max=100"`
-	Password     *string            `json:"password" validate:"required, min=8"`
+	FirstName    *string            `json:"firstName" validate:"required,min=2,max=100"`
+	LastName     *string            `json:"lastName" validate:"required,min=2,max=100"`
+	Password     *string            `json:"password" validate:"required,min=8"`
 	Email        *string            `json:"email" validate:"required"`
 	Phone        *string            `json:"phone" validate:"required"`
 	Token        *string            `json:"token"`
-	UserType     *string            `json:"userType" validate:"required, eq=ADMIN|eq=USER"`
+	UserType     *string            `json:"userType" validate:"required,eq=ADMIN|eq=USER"`
 	RefreshToken *string            `json:"refreshToken"`
 	CreatedAt    time.Time          `json:"createdAt"`
 	UpdatedAt    time.Time          `json:"updatedAt"`
