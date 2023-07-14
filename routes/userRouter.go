a6263c05fbff8b4a131a5e5af7979bf4ec6159e9
Lh <lihao1017@qq.com>
2023-07-14 17:31:50 +0800

    fix bug

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,14 +2,14 @@ package routes
 
 import (
 	"github.com/SiriusLLL/go-jwt/controllers"
-	//"github.com/SiriusLLL/go-jwt/middleware"
+	"github.com/SiriusLLL/go-jwt/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 // 用户相关的API路由
 func UserRoutes(incomingRoutes *gin.Engine) {
-	// incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:userId", controllers.GetUser())
 }
